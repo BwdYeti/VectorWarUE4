ee4a699585dade09a06503a13d2902edc4f84e62
BwdYeti <BwdYeti@gmail.com>
2020-05-24 21:49:59 -0400

    VectorWar
    -Add references to VectorWar module

--- Source/VectorWarUE4Editor.Target.cs
+++ Source/VectorWarUE4Editor.Target.cs
@@ -9,6 +9,6 @@ public class VectorWarUE4EditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		ExtraModuleNames.AddRange( new string[] { "VectorWarUE4" } );
+		ExtraModuleNames.AddRange( new string[] { "VectorWarUE4", "VectorWar" } );
 	}
 }
