d8375870b19f795d44306683e47d413913ef5177
BwdYeti <BwdYeti@gmail.com>
2020-05-26 01:46:21 -0400

    GGPO
    -Add a dependency to GGPOUE4

--- VectorWar.Build.cs
+++ VectorWar.Build.cs
@@ -1,5 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System.IO;
 using UnrealBuildTool;
 
 public class VectorWar : ModuleRules
@@ -7,14 +8,14 @@ public class VectorWar : ModuleRules
 	public VectorWar(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "GGPOUE4" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
