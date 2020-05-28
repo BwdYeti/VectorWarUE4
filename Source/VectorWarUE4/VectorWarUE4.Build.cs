6cf76a27795a4f4213b942a1451ae7b7910ce0cd
BwdYeti <BwdYeti@gmail.com>
2020-05-28 00:22:21 -0400

    Submodules
    -Commit VectorWar and GGPOUE4 submodule updates
    -Add dependencies

--- Source/VectorWarUE4/VectorWarUE4.Build.cs
+++ Source/VectorWarUE4/VectorWarUE4.Build.cs
@@ -10,7 +10,7 @@ public class VectorWarUE4 : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "GGPOUE4", "VectorWar" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
