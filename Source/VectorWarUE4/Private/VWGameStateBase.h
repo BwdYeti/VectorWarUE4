395bf7227d91988750dd71429607c9b7b2d7b13d
BwdYeti <BwdYeti@gmail.com>
2020-05-30 22:10:58 -0400

    Documentation

--- Source/VectorWarUE4/Private/VWGameStateBase.h
+++ Source/VectorWarUE4/Private/VWGameStateBase.h
@@ -22,12 +22,18 @@ public:
 
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
+	/**
+	 * Called from BeginPlay() after creating the game state.
+	 * Can be overridden by a blueprint to create actors that represent the game state.
+	 */
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="GGPO")
 	void OnSessionStarted();
 	virtual void OnSessionStarted_Implementation();
 
 private:
+	/** Starts a single player GGPO game session. */
 	HWND StartSinglePlayerGGPOSession();
+	/** Starts a GGPO game session. */
 	HWND StartGGPOPlayerSession(const uint16 LocalPort, const int32 NumPlayers, TArray<wchar_t*> PlayerParameters);
 
 	HWND Hwnd;
