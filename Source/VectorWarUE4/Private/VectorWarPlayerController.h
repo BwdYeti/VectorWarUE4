d223b055b6da312a96a0f7d71cc555eba17ed25f
BwdYeti <BwdYeti@gmail.com>
2020-06-06 19:46:33 -0400

    VectorWarPlayerController

---
+++ Source/VectorWarUE4/Private/VectorWarPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright 2020 BwdYeti.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "VectorWarPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AVectorWarPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
